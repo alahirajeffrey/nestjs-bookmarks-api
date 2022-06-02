6bf60a1ef185046069ac72589067ce47f2d70a4b
alahirajeffrey <alahirajeffrey@gmail.com>
2022-06-02 02:15:40 -0700

    setup typeorm

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,11 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { UserEntity } from "src/entities/user.entity";
+import { AuthController } from "./auth.controller";
+import { AuthService } from "./auth.service";
 
-@Module({})
+@Module({imports:[TypeOrmModule.forFeature([UserEntity])],
+    controllers:[AuthController],
+    providers:[AuthService]
+})
 export class AuthModule{}
