f4372fc80d783d35621b65e895cbe3a31a5f8682
alahirajeffrey <alahirajeffrey@gmail.com>
2022-06-02 01:36:42 -0700

    setup config and typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,25 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [AuthModule, UserModule, BookmarkModule],
+  imports: [
+    ConfigModule.forRoot({isGlobal:true}),
+    TypeOrmModule.forRoot({
+      type:'postgres',
+      host: process.env.PG_HOST,
+      username: 'postgres' || process.env.PG_USERNAME,
+      password: process.env.PG_PASSWORD,
+      database: process.env.PG_DATABASE,
+      port: parseInt(<string>process.env.PG_PORT),
+      autoLoadEntities: true,
+      synchronize: true
+    }),
+    AuthModule, 
+    UserModule, 
+    BookmarkModule],
   controllers: [],
   providers: [],
 })
