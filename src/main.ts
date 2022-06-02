7aac35e07f3bd95a7aa5ea555d0affd2061f5a19
alahirajeffrey <alahirajeffrey@gmail.com>
2022-06-02 02:14:28 -0700

    code refactor

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api')
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true
   }))
   await app.listen(8080);
 
-  app.setGlobalPrefix('api')
 }
 bootstrap();
