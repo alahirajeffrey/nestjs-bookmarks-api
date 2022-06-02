9ca3269d77544e831be90ecfbc6063b1fa58272b
alahirajeffrey <alahirajeffrey@gmail.com>
2022-06-02 04:37:37 -0700

    added login dto

---
+++ src/auth/dto/login.dto.ts
@@ -0,0 +1,11 @@
+import { IsEmail, IsNotEmpty } from "class-validator";
+
+export class LoginDto{
+
+    @IsEmail()
+    @IsNotEmpty()
+    username: string
+
+    @IsNotEmpty()
+    password: string
+}
