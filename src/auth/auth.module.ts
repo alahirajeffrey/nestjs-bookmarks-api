baf9836da24e1929b93378031a22a7a6fbadec32
alahirajeffrey <alahirajeffrey@gmail.com>
2022-06-02 04:27:00 -0700

    added bookmarks entity

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,11 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { BookmarkEntity } from "src/entities/bookmark.entity";
 import { UserEntity } from "src/entities/user.entity";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 
-@Module({imports:[TypeOrmModule.forFeature([UserEntity])],
+@Module({imports:[TypeOrmModule.forFeature([UserEntity, BookmarkEntity])],
     controllers:[AuthController],
     providers:[AuthService]
 })
