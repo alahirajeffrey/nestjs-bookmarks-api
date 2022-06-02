373121a9f30bc6c7dd5004b2ac9baf4882be3272
alahirajeffrey <alahirajeffrey@gmail.com>
2022-06-02 01:50:16 -0700

    updated entity

--- src/entities/bookmark.entity.ts
+++ src/entities/bookmark.entity.ts
@@ -1,6 +1,6 @@
 import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
-@Entity()
+@Entity('bookmarks')
 export class BookmarkEntity{
     @PrimaryGeneratedColumn('uuid')
     id:string
