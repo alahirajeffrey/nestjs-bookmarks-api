621b3a140d3864452b18c0c74995814cdc942c22
alahirajeffrey <alahirajeffrey@gmail.com>
2022-06-03 23:49:10 -0700

    imported and registered jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,16 @@
 import { Module } from "@nestjs/common";
+import { JwtModule } from "@nestjs/jwt";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { BookmarkEntity } from "src/entities/bookmark.entity";
 import { UserEntity } from "src/entities/user.entity";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
+import { JwtStrategy } from "./strategy/index";
 
-@Module({imports:[TypeOrmModule.forFeature([UserEntity, BookmarkEntity])],
+
+@Module({imports:[JwtModule.register({}),
+    TypeOrmModule.forFeature([UserEntity, BookmarkEntity])],
     controllers:[AuthController],
-    providers:[AuthService]
+    providers:[AuthService, JwtStrategy]
 })
 export class AuthModule{}
