a8a0d165cf665b779a0ced1a360ad8950e42666b
alahirajeffrey <alahirajeffrey@gmail.com>
2022-06-09 14:40:08 -0700

    imported services and controllers

--- src/bookmark/bookmark.module.ts
+++ src/bookmark/bookmark.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BookmarkEntity } from 'src/entities/bookmark.entity';
+import { UserEntity } from 'src/entities/user.entity';
+import { BookmarkController } from './bookmark.controller';
+import { BookmarkService } from './bookmark.service';
 
-@Module({})
+@Module({
+    imports: [TypeOrmModule.forFeature([UserEntity, BookmarkEntity])],
+    providers:[BookmarkService],
+    controllers:[BookmarkController]
+})
 export class BookmarkModule {}
