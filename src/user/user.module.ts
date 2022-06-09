c106ac665daa55d95b11f1608ba1460ba561fd8a
alahirajeffrey <alahirajeffrey@gmail.com>
2022-06-09 14:30:49 -0700

    imported services and controllers

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,9 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { BookmarkEntity } from 'src/entities/bookmark.entity';
 import { UserEntity } from 'src/entities/user.entity';
 import { UserController } from './user.controller';
+import { UserService } from './user.service';
 
 @Module({
     imports:[TypeOrmModule.forFeature([UserEntity, BookmarkEntity])],
-    controllers:[UserController]
+    controllers:[UserController],
+    providers:[UserService]
 })
 export class UserModule {}
